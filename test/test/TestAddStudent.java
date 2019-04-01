59df1afdb0bf20e898a38b51501f70d4a1aaa8a4
ChisIulia <ciie2089@scs.ubbcluj.ro>
2019-04-01 20:37:42 +0300

    Refactored tests.

--- test/test/TestAddStudent.java
+++ test/test/TestAddStudent.java
@@ -10,11 +10,11 @@ import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 
-public class AddingTests {
+public class TestAddStudent {
     private StudentValidator studentValidator = new StudentValidator();
 
     @Test
-    public void addStudentTest() {
+    public void testCorrectStudent() {
         List<Student> studentList = new LinkedList<>();
 
         String id = "1";
@@ -34,24 +34,18 @@ public class AddingTests {
         assertEquals(1, studentList.size());
     }
 
-    @Test
-    public void addStudentTest2() {
+    @Test(expected = ValidatorException.class)
+    public void testMissingId() throws ValidatorException {
         List<Student> studentList = new LinkedList<>();
 
-        String id = "1";
+        String id = "";
         String nume = "Iulia";
         Integer grupa = 932;
         String email = "a";
         String prof = "profa";
         Student student = new Student(id, nume, grupa, email, prof);
 
-        try {
-            studentValidator.validate(student);
-            studentList.add(student);
-        } catch (ValidatorException e) {
-            e.printStackTrace();
-        }
-
-        assertEquals(1, studentList.size());
+        studentValidator.validate(student);
+        studentList.add(student);
     }
 }
